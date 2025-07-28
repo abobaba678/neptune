7369c8ecdcbf19f65a1e6cb525684f2a37130f54
abobaba678 <kumusbaevanv@gmail.com>
2025-07-28 15:45:20 +0500

    idk, just fix

--- neptune.py
+++ neptune.py
@@ -15,7 +15,7 @@ class App(CTk.CTk):
             self.image1 = CTk.CTkLabel(app, image=self.photo1, text="")
             self.image1.place(x=0, y=140)
 
-        def inputdio():
+        def input():
             file_path = filedialog.askopenfilename(
               filetypes=[("All files", "*.*")]
             )
@@ -27,7 +27,7 @@ class App(CTk.CTk):
                     self.img = self.img.resize((int(self.entryX2.get()), int(self.entryY2.get())), Image.Resampling.LANCZOS)
                     update()
                 except Exception as e:
-                    print(f"Ошибка при открытии файла: {str(e)}")
+                    print(f"error: {str(e)}")
         def filter():
             op1 = self.optionmenu_var.get()
             op2 = getattr(ImageFilter, op1)()
@@ -46,12 +46,9 @@ class App(CTk.CTk):
         def save():
             self.img.save(self.entrySave.get())
         
-        #пустая строчкаg
 
 
-
-
-        self.title("neptunee")
+        self.title("neptune")
         self.geometry("700x400")
         self.resizable(True, True)
         try:
@@ -65,7 +62,7 @@ class App(CTk.CTk):
 
 
         self.optionmenu_var = CTk.StringVar(value="BLUR")
-        self.button1 = CTk.CTkButton(self, text="openimage", command=inputdio)
+        self.button1 = CTk.CTkButton(self, text="openimage", command=input)
         self.button1.grid(row=0, column=0, sticky="s", padx=5, pady=5)
         self.button2 = CTk.CTkButton(self, text="rotate", command=rotate)
         self.button2.grid(row=0, column=1, sticky="s", padx=5, pady=5)
@@ -73,11 +70,11 @@ class App(CTk.CTk):
         self.entryX.grid(row=1, column=1, sticky="s", padx=5, pady=5)
         self.entryY = CTk.CTkEntry(self, placeholder_text="Y")
         self.entryY.grid(row=2, column=1, sticky="s", padx=5, pady=5)
-        self.button3 = CTk.CTkButton(self, text="применить фильтр", command=filter)
+        self.button3 = CTk.CTkButton(self, text="apply filter", command=filter)
         self.button3.grid(row=1, column=0, sticky="s", padx=5, pady=5)
-        self.entryX2 = CTk.CTkEntry(self, placeholder_text="X открытой картнки")
+        self.entryX2 = CTk.CTkEntry(self, placeholder_text="X image")
         self.entryX2.grid(row=0, column=2, sticky="s", padx=5, pady=5)
-        self.entryY2 = CTk.CTkEntry(self, placeholder_text="Y открытой картинки")
+        self.entryY2 = CTk.CTkEntry(self, placeholder_text="Y image")
         self.entryY2.grid(row=1, column=2, sticky="s", padx=5, pady=5)
         self.opitonb1 = CTk.CTkOptionMenu(master=self, values=["BLUR",
 "CONTOUR",
@@ -91,13 +88,13 @@ class App(CTk.CTk):
 "SMOOTH_MORE"], variable=self.optionmenu_var)
         self.opitonb1.grid(row=2, column=0, padx=5, pady=5, sticky="nsew")
 
-        self.button4 = CTk.CTkButton(self, text="вставить", command=paste)
+        self.button4 = CTk.CTkButton(self, text="paste", command=paste)
         self.button4.grid(row=2, column=2, sticky="s", padx=5, pady=5)
 
         self.button5 = CTk.CTkButton(self, text="save", command=save)
         self.button5.grid(row=0, column=3, sticky="s", padx=5, pady=5)
 
-        self.entrySave = CTk.CTkEntry(self, placeholder_text="имя дял сохранённой картинки")
+        self.entrySave = CTk.CTkEntry(self, placeholder_text="name for image")
         self.entrySave.grid(row=1, column=3, sticky="s", padx=5, pady=5)
 if __name__ == "__main__":
     app = App()
